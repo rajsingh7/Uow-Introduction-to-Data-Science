e85a26e0d447bacc45a3893da412cbdc9489f4b1
RyanCheungGit <ryanzjlib@gmail.com>
2014-07-20 10:08:45 +0800

    part2 matrix multiplication in SQL updata

--- assignment2/prob7.sql
+++ assignment2/prob7.sql
@@ -1,6 +1,6 @@
-SELECT row_num,col_num, SUM(value) AS value
-FROM(
-  SELECT A.row_num, B.col_num, A.value*B.value AS value
-  FROM A JOIN B
-  ON A.col_num=B.row_num)
-GROUP BY row_num, col_num;
+select row_num,col_num, sum(value) as value
+from(
+  select A.row_num, B.col_num, A.value*B.value as value
+  from A join B
+  on A.col_num=B.row_num)
+group by row_num, col_num;
